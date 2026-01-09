0556b096b78c0498faa7a1942a581d9fcb6bd906
zihan <lzzihan@email.cn>
2026-01-10 02:08:54 +0800

    更新

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -55,7 +55,7 @@ const links3 = ref([{
     }
     ,{
         title: "gitee",
-        href: "https://gitee.com/"
+        href: "https://github.com/zihani"
     },{
         title: "掘金",
         href: "https://juejin.cn/"
@@ -98,9 +98,9 @@ const banner = useBannerStore()
                     <div v-for="(item,i) in links3" :key="i"><a :href="item.href" target="_blank">{{item.title}}</a></div>
                 </div>
             </div>
-            <!-- <div class="copyright">Copyright © 2026 by <a target="_blank" class="out-link" href=""></a> . All rights reserved. | <a target="_blank" class="out-link" href="http://www.beian.miit.gov.cn">京ICP备2023036653号-1</a></div> -->
+            <div class="copyright">Copyright © 2023 by <a target="_blank" class="out-link" href=""></a> . All rights reserved. | <a target="_blank" class="out-link" href="http://www.beian.miit.gov.cn">京ICP备2023036653号-1</a></div>
         </div>
-    </div>  
+    </div>
 </template>
 <style scoped>
 #layout-footer{
